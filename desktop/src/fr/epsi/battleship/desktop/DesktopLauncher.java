eaeed95de842415c4990a216d94cc1b10cb6e62d
DEBICI Sébastien <sebastien.debici@gmail.com>
2016-06-03 11:04:14 +0200

    Add Icon

--- desktop/src/fr/epsi/battleship/desktop/DesktopLauncher.java
+++ desktop/src/fr/epsi/battleship/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package fr.epsi.battleship.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import fr.epsi.battleship.Battleship;
@@ -10,6 +11,7 @@ public class DesktopLauncher {
 		config.title = "Battleship";
         config.width = 800;
         config.height = 480;
+		config.addIcon("core/assets/icon.png", Files.FileType.Internal);
 		new LwjglApplication(new Battleship(), config);
 	}
 }
